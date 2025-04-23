a3384acfe23d852e794fae4780b1ec523320dbf3
BLANC Alexis <blanc.alexispro@gmail.com>
2025-04-23 20:37:26 +0200

    feat(user_screen): add ui component

--- app/src/main/java/fr/ablanc/fileexchangeandroid/data/di/AppModule.kt
+++ app/src/main/java/fr/ablanc/fileexchangeandroid/data/di/AppModule.kt
@@ -9,6 +9,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.websocket.WebSockets
+import org.koin.core.context.loadKoinModules
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
@@ -18,13 +19,14 @@ fun injectFeatures() = loadFeatures
 
 
 private val loadFeatures by lazy {
-    listOf(
-        appModule
+    loadKoinModules(
+        listOf(
+            appModule,
+        )
     )
 }
 
 val appModule = module {
-    viewModelOf(::BaseViewModel)
 
     single {
         HttpClient(CIO) {
@@ -34,6 +36,7 @@ val appModule = module {
     }
     single<SocketDataSource> { SocketDataSourceImpl(get(), SERVER_ADDRESS) }
     single<SocketRepository> { SocketRepositoryImpl(get()) }
+    viewModelOf(::BaseViewModel)
 
 }
 
--- app/src/main/java/fr/ablanc/fileexchangeandroid/presentation/BaseViewModel.kt
+++ app/src/main/java/fr/ablanc/fileexchangeandroid/presentation/BaseViewModel.kt
@@ -1,21 +1,79 @@
 package fr.ablanc.fileexchangeandroid.presentation
 
+import android.util.Log.d
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import fr.ablanc.fileexchangeandroid.domain.SocketRepository
+import fr.ablanc.fileexchangeandroid.domain.util.Ressource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.NonCancellable.isActive
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class BaseViewModel(
     private val repository: SocketRepository
 ) : ViewModel() {
 
+    private val job : Job = socketConnection()
+
     private val _state = MutableStateFlow(BaseState())
     val state = _state.onStart {
-
     }.stateIn(
         viewModelScope, SharingStarted.WhileSubscribed(5000), BaseState()
     )
+
+    private fun setServerConnected(state: Boolean){
+        _state.update { it.copy(serverConnected = state) }
+    }
+
+    private suspend fun socketConnect(){
+        if (this.job.isActive == true) {
+            socketDisconnection()
+        } else {
+            job.start()
+        }
+    }
+
+    private fun socketConnection() : Job {
+        return viewModelScope.launch {
+            repository.connect().collect {
+                when (it) {
+                    is Ressource.Error<*> -> setServerConnected(false)
+                    is Ressource.Loading<*> -> println("co : loading")
+                    is Ressource.Success<*> -> {
+                        println("co : success")
+                        setServerConnected(true)
+                        socketListen()
+                    }
+                }
+            }
+        }
+
+    }
+    private suspend fun socketDisconnection(){
+        repository.disconnect()
+            .onSuccess {
+                job.cancel()
+            }
+            .onFailure {
+
+            }
+    }
+    private suspend fun socketListen(){
+        repository.listen().collect{
+            when(it){
+                is Ressource.Error<*> -> println() // Gestion erreur
+                is Ressource.Loading<*> -> println()
+                is Ressource.Success<*> -> println(it.data)
+            }
+        }
+    }
+    private fun socketSend(){
+
+    }
+
 }
